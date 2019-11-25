828f6696d44cc7b93c6e1c67f43e764cac519c72
Huijie Wei <huijiewei@outlook.com>
2019-11-25 11:03:13 +0800

    更新

--- agile-boot-admin-api/src/main/java/com/huijiewei/agile/boot/admin/api/config/DateFormatConfig.java
+++ agile-boot-admin-api/src/main/java/com/huijiewei/agile/boot/admin/api/config/DateFormatConfig.java
@@ -18,7 +18,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
-@ConfigurationProperties(prefix = "agile")
+@ConfigurationProperties(prefix = "agile.format")
 public class DateFormatConfig {
     private String dateFormat = "yyyy-MM-dd";
 
