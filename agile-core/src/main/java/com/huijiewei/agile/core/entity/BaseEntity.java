aae3816097aed998378fc7ca53234ffa5ad617ad
Huijie Wei <huijiewei@outlook.com>
2019-12-04 21:15:38 +0800

    更改主键方式

--- agile-core/src/main/java/com/huijiewei/agile/core/entity/BaseEntity.java
+++ agile-core/src/main/java/com/huijiewei/agile/core/entity/BaseEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     public Boolean hasId() {
