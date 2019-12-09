498ddeec8a81d9003698badaada3edc8b43f4f3f
Huijie Wei <huijiewei@outlook.com>
2019-12-10 00:28:38 +0800

    修复表名错误

--- agile-core-shop/src/main/java/com/huijiewei/agile/core/shop/entity/ShopCategory.java
+++ agile-core-shop/src/main/java/com/huijiewei/agile/core/shop/entity/ShopCategory.java
@@ -7,18 +7,14 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
-@Table(name = ShopCategory.TABLE_NAME)
 @DynamicInsert
 @DynamicUpdate
 public class ShopCategory extends TreeEntity<ShopCategory> {
-    public static final String TABLE_NAME = "ag_shop_category";
-
     @NotBlank
     private String name;
 
