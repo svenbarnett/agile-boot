a0ed635e6f982243fa07a3e6351feb67dbb125a6
Huijie Wei <huijiewei@outlook.com>
2019-12-25 19:17:05 +0800

    优化代码

---
+++ agile-serve-admin/src/main/java/com/huijiewei/agile/serve/admin/controller/ShopProductController.java
@@ -0,0 +1,9 @@
+package com.huijiewei.agile.serve.admin.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "shop-product", description = "商品接口")
+public class ShopProductController {
+}
