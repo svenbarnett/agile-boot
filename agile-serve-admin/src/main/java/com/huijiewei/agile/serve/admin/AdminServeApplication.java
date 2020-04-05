0b51647ea55591b20e2718fe08e9b623f96f4260
Huijie Wei <huijiewei@outlook.com>
2020-04-05 17:27:20 +0800

    精简代码

--- agile-serve-admin/src/main/java/com/huijiewei/agile/serve/admin/AdminServeApplication.java
+++ agile-serve-admin/src/main/java/com/huijiewei/agile/serve/admin/AdminServeApplication.java
@@ -1,5 +1,6 @@
 package com.huijiewei.agile.serve.admin;
 
+import com.cosium.spring.data.jpa.entity.graph.repository.support.EntityGraphJpaRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -7,10 +8,12 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @ComponentScan(basePackages = "com.huijiewei.agile")
 @EntityScan(basePackages = "com.huijiewei.agile")
+@EnableJpaRepositories(basePackages = "com.huijiewei.agile", repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class)
 @EnableCaching
 public class AdminServeApplication {
     public static void main(String[] args) {
